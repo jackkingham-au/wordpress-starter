29fab685f7c3f98500917451d204ef9b90ddc256
Jack <jack@jkdigital.info>
2021-04-07 18:43:14 +1000

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,10 @@
+</main>
+<footer>
+
+</footer>
+<!-- Materialize JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<!-- Bundled JS -->
+<script src="dist/bundled.min.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Starter PHP</title>
+    <!-- Materialize CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Material Design Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="dist/style.css">
+</head>
+<body>
+<header>
+
+</header>
+<main>
